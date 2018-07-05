2018a3ba5af1676506fe1cc4644181270fed6f7a
Martin <hic0811@gmail.com>
2018-07-05 19:26:06 -0400

    In body tag, Grid-template-rows: 40px 500px 500px 20px

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>DevNotes</title>
+        <link rel="stylesheet" type="text/css" href="devnotes.css">
+    </head>
+
+    <body>
+        <header>
+            DevNotes
+        </header>
+
+        <aside>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+        </aside>
+
+        <main>
+            <header>
+                <h1>Welcome</h1>
+            </header>
+
+            <div id="core">
+                <section>
+                    Hello Martin!
+                </section>
+
+                <section>
+                    Good work!
+                </section>
+            </div>
+        </main>
+
+        <footer>
+            &copy; 2018 Inchul Hwang
+        </footer>
+    </body>
+</html>
