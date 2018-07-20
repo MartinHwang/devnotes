56c79a0d812f0382d33f89e5db056e857730c2bd
Martin <hic0811@gmail.com>
2018-07-19 21:44:41 -0400

    Add basic front controller

---
+++ base.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>DevNotes</title>
+        <link rel="stylesheet" type="text/css" href="/devnotes.css">
+    </head>
+
+    <body>
+        <header>
+            <h3>Header</h3>
+        </header>
+
+        <aside>
+            <nav>
+                <p>Aside</p>
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/notes-css">CSS Notes</a></li>
+                    <li><a href="/notes-js">JavaScript Notes</a></li>
+                </ul>
+            </nav>
+        </aside>
+
+        <main>
+            <?php include $request . '.php'; ?>
+        </main>
+
+        <footer>
+            &copy; 2018 Inchul Hwang
+        </footer>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,42 +1,19 @@
-<!DOCTYPE html>
+<?php
 
-<html>
-    <head>
-        <title>DevNotes</title>
-        <link rel="stylesheet" type="text/css" href="devnotes.css">
-    </head>
+//echo '<pre>' . print_r(explode('/', trim($_SERVER["REQUEST_URI"], '/')), true) . '</pre>';
 
-    <body>
-        <header>
-            <h3>Header</h3>
-        </header>
+$request = end(explode('/', trim($_SERVER["REQUEST_URI"], '/')));
 
-        <aside>
-            <nav>
-                <p>Aside</p>
-                <ul>
-                    <li><a href="index.php?p=home">Home</a></li>
-                    <li><a href="index.php?p=notes-css">CSS Notes</a></li>
-                    <li><a href="index.php?p=notes-js">JavaScript Notes</a></li>
-                </ul>
-            </nav>
-        </aside>
+switch ($request) {
+    case 'home':
+        break;
+    case 'notes-css':
+        break;
+    case 'notes-js':
+        break;
+    default:
+        $request = 'error';
+        break;
+}
 
-        <main>
-            <?php
-            $validPages = ['home', 'notes-css', 'notes-js'];
-            $page = $_GET['p'];
-            
-            if (in_array($page, $validPages)) {
-                include($page . '.html');
-            } else {
-                echo "Invalid page.";
-            }
-            ?>
-        </main>
-
-        <footer>
-            &copy; 2018 Inchul Hwang
-        </footer>
-    </body>
-</html>
+include 'base.php';
