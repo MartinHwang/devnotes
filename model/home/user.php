23f01051fe7795e9e4330f9b6ddf9ed2b6f40431
Martin <hic0811@gmail.com>
2018-08-11 18:02:47 -0400

    Switch front-controller to OOP; add Symfony Dotenv

--- model/home/user.php
+++ model/home/user.php
@@ -5,8 +5,14 @@
  */
 function getUsers()
 {
+    $dbHost = getenv('DB_HOST');
+    $dbPort = getenv('DB_PORT');
+    $dbName = getenv('DB_NAME');
+    $dbUser = getenv('DB_USER');
+    $dbPass = getenv('DB_PASS');
+
     // Connecting, selecting database
-    $dbconn = pg_connect("host='localhost' port=5432 dbname='devnotes' user='devnotes' password='devnotes'")
+    $dbconn = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPass")
     or die('Could not connect: ' . pg_last_error());
 
     // Performing SQL query
--- public/index.php
+++ public/index.php
@@ -2,21 +2,30 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$requestParts = explode('/', trim($_SERVER["REQUEST_URI"], '/'));
-$request      = end($requestParts);
+use Symfony\Component\Dotenv\Dotenv;
 
-switch ($request) {
-    case '':
-    case 'home':
-        include '../controller/home/HomeController.php';
-        break;
-    case 'notes-css':
-        include '../controller/notes-css/NotesCSSController.php';
-        break;
-    case 'notes-js':
-        include '../controller/notes-js/NotesJSController.php';
-        break;
-    default:
-        include '../controller/DefaultController.php';
-        break;
+class app
+{
+
+    public function router()
+    {
+        $requestParts = explode('/', trim($_SERVER["REQUEST_URI"], '/'));
+        $request      = end($requestParts);
+
+        switch ($request) {
+            case '':
+            case 'home':
+                return '../controller/home/HomeController.php';
+            case 'notes-css':
+                return '../controller/notes-css/NotesCSSController.php';
+            case 'notes-js':
+                return '../controller/notes-js/NotesJSController.php';
+            default:
+                return '../controller/DefaultController.php';
+        }
+    }
 }
+
+(new Dotenv())->load(__DIR__ . '/../.env');
+
+include_once (new app())->router();
